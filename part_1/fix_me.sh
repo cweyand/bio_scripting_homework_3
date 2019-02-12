794eaa683584dc807066cd108d9817e5eb382b4a
Courtney Weyand <courtney.weyand@auburn.edu>
2019-02-11 18:32:22 -0600

    changed read -p lines to have double quotes

--- part_1/fix_me.sh
+++ part_1/fix_me.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Reading data from the user's input
-read -p 'Enter a:' a
-read -p 'Enter b:' b
+read -p "Enter a:" a
+read -p "Enter b:" b
 
 	add=$((a + b))
         echo Addition of a and b are $add
