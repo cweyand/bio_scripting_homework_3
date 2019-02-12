594919013d827586145a7bca8e926cf04a086c43
Courtney Weyand <courtney.weyand@auburn.edu>
2019-02-11 19:28:16 -0600

    added command to tell us the date

--- part_2/sys_info_WEYAND.sh
+++ part_2/sys_info_WEYAND.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
 time=`date | awk '{print $4}'`
 echo "The time is ${time}."
+
+#date formatting Mon Feb 11 19:01:49 CST 2019
+day=`date | awk '{print $2 $3 $6}'`
+echo "Today is ${day}."
