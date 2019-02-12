df4a04dedf74dfd8f5779be05a08a6e2b242ee3c
Courtney Weyand <courtney.weyand@auburn.edu>
2019-02-11 18:27:43 -0600

    changed spacing reformatted commands

--- part_1/fix_me.sh
+++ part_1/fix_me.sh
@@ -5,22 +5,19 @@ read -p 'Enter a:' a
 read -p 'Enter b:' b
 
 	add=$((a + b))
-        echo "Addition of a and b are ${add}"
+        echo Addition of a and b are $add
 
-            sub=$((a - b))
-        echo "Subtraction of a and b are ${sub}"
+        sub=$((a - b))
+        echo Subtraction of a and b are $sub
 
-            mul=$((a * b))
-        echo "Multiplication of a and b are ${mul}"
+        mul=$((a * b))
+        echo Multiplication of a and b are $mul
 
-            div
-    = $((a / d))
+        div=$((a / d))
         echo division of a and b are $div
 
-            mod
-    = $((a % b))
-          echo Modulus of a
-      and b are $mod
+       mod=$((a % b))
+        echo Modulus of a and b are $mod
 
       ((++a))
           echo Increment
